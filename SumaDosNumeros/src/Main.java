84ca13ace0eeb9cac12300a04c71a875b172cd30
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-16 10:03:25 -0500

    Update Main.java

--- SumaDosNumeros/src/Main.java
+++ SumaDosNumeros/src/Main.java
@@ -1,11 +1,9 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("SUMA DE DOS NUMEROS");
-
         Scanner reader=new Scanner(System.in);
 
         //variables
@@ -25,9 +23,6 @@ public class Main {
 
         //Presentar resultado
         System.out.println("La suma de los numeros es :"+ numero1 + "+"+ numero2 + "=" + resultado);
-
-
-
 
     }
 }
