fa65d99bae169fdf8b948d6123f09c5c0118ef0d
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-16 22:23:18 -0500

    Update Main.java

--- VerificacionNumero/src/Main.java
+++ VerificacionNumero/src/Main.java
@@ -1,19 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("VERIFICAR NUMERO ");
         Scanner reader = new Scanner(System.in);
 
-        //variable
+        //Variable
         int numero;
 
         //Ingresar el numero
         System.out.println("Ingrese el numero a verificar:");
         numero = reader.nextInt();
 
-        //verificar numero si es par o impar
+        //Verificar numero si es par o impar
         if (numero % 2 == 0) {
             System.out.println("El numero "+ numero +" es par ");
 
