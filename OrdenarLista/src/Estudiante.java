3a918d7f012bf0b88410b19d3f7e3854f8a313ab
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-06-16 19:45:06 -0500

    Prueba 1

---
+++ OrdenarLista/src/Estudiante.java
@@ -0,0 +1,20 @@
+
+public class Estudiante {
+    private String nombre;
+    private int nota;
+
+    public Estudiante(String nombre, int nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+}
+
+
