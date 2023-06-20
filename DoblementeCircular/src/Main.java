ecff87091051185fe05136a519fc1ece53f4a345
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-19 23:31:55 -0500

    Update Main.java

--- DoblementeCircular/src/Main.java
+++ DoblementeCircular/src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         Lista lista = new Lista();
+        
         // Agregar valores a la lista
         lista.agregar(3);
         lista.agregar(5);
@@ -13,8 +14,10 @@ public class Main {
 
         System.out.println("Lista original:");
         lista.mostrarLista();
+        
         // Insertar un nodo después del nodo con valor 10
         lista.insertarNodo(7, 10);
+       
         // Eliminar el nodo con valor 5
         lista.eliminarNodo(5);
         lista.mostrarLista();
