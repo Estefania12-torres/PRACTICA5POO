728a8dcb411fd5531901d21e7e11f55b0d01ddf3
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-19 23:37:00 -0500

    Update Nodo.java

--- ListaCircular/src/Nodo.java
+++ ListaCircular/src/Nodo.java
@@ -1,11 +1,14 @@
 class Nodo {
+    //atributos
     private int dato;
     private Nodo siguiente;
-
+    
+    //constructor
     public Nodo(int dato) {
         this.dato = dato;
         this.siguiente = null;
     }
+    //getters y setters
 
     public int getDato() {
         return dato;
